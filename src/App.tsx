5475c0bcd197a1450513da9e83f0257b861627ce
ghazal <ghazal.abaszade1@gmail.com>
2023-05-10 13:26:03 +0330

    added comments component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import pizza from "./assets/pizza.jpg";
+import {useEffect, useState} from "react";
+import Comments from "./Components/comments";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [comments, setComments] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const fetchData = async () => {
+        setLoading(true);
+        const response = await fetch('https://react-mini-projects-api.classbon.com/Comments/1');
+        const data = await response.json()
+
+        setComments(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+    return (
+        <div className="container pt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={pizza} />
+                </div>
+                <div className="col-md-6 pt-4">
+                    <div className="product-details pb-3">
+                        <div className="mb-3">
+                          <span className="h3 fw-normal text-accent">
+                            قیمت: 158,000
+                          </span>
+                        </div>
+                        <form className="mb-grid-gutter">
+                            <div className="row mx-n2">
+                                <div className="col-6 px-2">
+                                    <div className="mb-3">
+                                        <label className="form-label">
+                                            اندازه:
+                                        </label>
+                                        <select className="form-select">
+                                            <option value="small">کوچک</option>
+                                            <option value="medium">متوسط</option>
+                                            <option value="large">بزرگ</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-6">
+                                    <div className="mb-3">
+                                        <label className="form-label">
+                                            تعداد:
+                                        </label>
+                                        <select className="form-select">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button
+                                className="btn btn-info btn-shadow d-block w-100"
+                                type="submit"
+                            >
+                                افزودن به سبد خرید
+                            </button>
+
+                        </form>
+                        <h5 className="h6 mb-3 pb-3 border-bottom">
+                            پیتزا استیک (یک نفره)
+                        </h5>
+                        <h6 className="fs-sm mb-2">ترکیبات:</h6>
+                        <p className="fs-sm">
+                            خمیر پیتزا کلاسیک ۲۶ سانتی متری، استیک فیله گوشت، پنیر، قارچ، فلفل
+                            دلمه ای، سس کچاپ مخصوص
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-12 pt-5">
+                    {
+                        loading ? (
+                            <div className="d-flex justify-content-center">
+                                <div className="spinner-border">
+
+                                </div>
+                            </div>
+                        ) : (
+                            comments.map(comment =>
+                            <Comments key={comment.id} {...comment} />
+                                )
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Components/type.ts
@@ -0,0 +1,10 @@
+export interface CommentsProps {
+    name: string;
+    imageUrl: string;
+    description: string;
+    date: string;
+}
+
+export interface items {
+    items : Array<CommentsProps>;
+}
